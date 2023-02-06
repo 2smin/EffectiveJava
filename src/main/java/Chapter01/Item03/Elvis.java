ab3da625fdc9cca79eef2ee9d49c0773a6bc9081
GyuHyoung <do0ob1124@gmail.com>
2023-02-06 23:04:40 +0900

    Item03
    - Singleton
      - private 생성자나 열거 타입으로 싱글턴임을 보증하라

---
+++ src/main/java/Chapter01/Item03/Elvis.java
@@ -0,0 +1,16 @@
+package Chapter01.Item03;
+
+public class Elvis {
+
+//    Elvis01 elvis01 = new Elvis01(); // 오류 발생
+
+    Elvis01 elvis01 = Elvis01.INSTANCE; //static으로 선언된 멤버 변수 접근하여 인스턴스 가져옴
+
+//    Elvis02 elvis02 = new Elvis02(); // 오류 발생
+
+    Elvis02 elvis02 = Elvis02.getInstance(); // getInstance라는 static 메소드를 통해 인스턴스 가져옴
+
+//    Elvis03 elvis03 = new Elvis03();  // 오류 발생 - enum이라 생성 불가
+
+    Elvis03 elvis03 = Elvis03.INSTANCE;
+}
