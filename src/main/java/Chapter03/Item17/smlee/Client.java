8ae5f3e12083d6f03bf151b263c2e9ce4c9d3a9e
2smin <planner263@gmail.com>
2023-02-14 22:19:26 +0900

    add item17 sample code

---
+++ src/main/java/Chapter03/Item17/smlee/Client.java
@@ -0,0 +1,22 @@
+package Chapter03.Item17.smlee;
+
+import java.math.BigInteger;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        BigInteger bigInteger = new BigInteger( "5");
+
+        BigInteger negateBigInteger = bigInteger.negate();
+
+        System.out.println(bigInteger);
+        System.out.println(negateBigInteger);
+
+
+    }
+
+    public static BigInteger getSafeInstance(BigInteger o){
+        return o.getClass() == BigInteger.class ? o : new BigInteger(o.toByteArray());
+    }
+}
---
+++ src/main/java/Chapter03/Item17/smlee/Complex.java
@@ -0,0 +1,30 @@
+package Chapter03.Item17.smlee;
+
+public class Complex {
+
+    private final double x;
+    private final double y;
+
+    private Complex(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    };
+
+    //방어적 복사
+    public Complex plus(Complex c){
+        return new Complex(x + c.x, y + c.y);
+    }
+
+    public static Complex getInstance(double x, double y){
+        return new Complex(x,y);
+    }
+
+}
