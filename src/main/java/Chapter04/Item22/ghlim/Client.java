015c3c283f83ad651a93e6d37e47e27f69242f99
GyuHyoung <do0ob1124@gmail.com>
2023-02-20 22:06:15 +0900

    Item 22
    - 인터페이스는 타입을 정의하는 용도로만 사용하라

---
+++ src/main/java/Chapter04/Item22/ghlim/Client.java
@@ -0,0 +1,9 @@
+package Chapter04.Item22.ghlim;
+
+
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Chapter04/Item22/ghlim/RepositoryImpl.java
@@ -0,0 +1,34 @@
+package Chapter04.Item22.ghlim;
+
+public class RepositoryImpl implements Repository{
+
+    @Override
+    public Integer getInt() {
+        return 1;
+    }
+
+    @Override
+    public Double getDouble() {
+        return 1.1;
+    }
+
+    @Override
+    public String getString() {
+        return "null";
+    }
+
+    @Override
+    public void setInt() {
+
+    }
+
+    @Override
+    public void setDouble() {
+
+    }
+
+    @Override
+    public void setString() {
+
+    }
+}
