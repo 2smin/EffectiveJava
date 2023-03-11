732f97b9cf57620860464a3f966a8bfb1e987d4e
2smin <planner263@gmail.com>
2023-03-11 22:30:09 +0900

    add item45 sample code

---
+++ src/main/java/Chapter07/Item45/SortAlphabet.java
@@ -0,0 +1,31 @@
+package Chapter07.Item45;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class SortAlphabet {
+
+    private int[] number;
+    private int limit;
+
+    public SortAlphabet(int[] number, int limit) {
+        this.number = number;
+        this.limit = limit;
+    }
+
+    public void sort(){
+        IntStream stream = Arrays.stream(number);
+        stream
+                .filter(number -> number <= limit)
+                .sorted()
+                .forEach(System.out::println);
+
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{4,2,7,9,6};
+        SortAlphabet sortAlphabet = new SortAlphabet(array,8);
+        sortAlphabet.sort();
+    }
+}
