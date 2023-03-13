c360114d99fd3ee39153e7cbbf90498a97c02bc8
2smin <planner263@gmail.com>
2023-03-13 21:35:30 +0900

    add item 47 sample code

---
+++ src/main/java/Chapter07/Item47/Client.java
@@ -0,0 +1,26 @@
+package Chapter07.Item47;
+
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void run1(){
+        for(ProcessHandle ph : (Iterable<? extends ProcessHandle>) ProcessHandle.allProcesses()::iterator){
+            // do ph
+        }
+    }
+
+    public static <E> Iterable<E> adapter(Stream<E> stream) {
+        return stream::iterator;
+    }
+
+    public static <E> Stream<E> streamOf(Iterable<E> iterable){
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
+}
