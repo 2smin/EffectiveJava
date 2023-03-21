9f3e1af440c72cb4c1dc36dfd61af861b94c747b
GyuHyoung <do0ob1124@gmail.com>
2023-03-21 21:12:50 +0900

    add item 57, 58

---
+++ src/main/java/Chapter09/Item58/ghlim/Client.java
@@ -0,0 +1,22 @@
+package Chapter09.Item58.ghlim;
+
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.Iterator;
+
+public class Client {
+    enum Face { ONE, TWO, THREE, FOUR, FIVE, SIX }
+    public static void main(String[] args){
+
+        Collection<Face> faces = EnumSet.allOf(Face.class);
+
+        for (Iterator<Face> i = faces.iterator(); i.hasNext(); )
+            for (Iterator<Face> j = faces.iterator(); j.hasNext(); ) {
+                System.out.println(i.next() + " " + j.next());
+            }
+
+        System.out.println(" ");
+//                System.out.println(i.next() + " " + j.next());
+    }
+
+}
